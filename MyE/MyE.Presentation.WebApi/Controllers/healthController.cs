223fde895a16f7ce5c0559bab9eb6e8a4cd45875
Gonzalo Escudero <gescudero@solera.pe>
2020-06-25 16:51:41 -0500

    healthStatus

---
+++ MyE/MyE.Presentation.WebApi/Controllers/healthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyE.Presentation.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class healthController : BaseApiController
+    {
+        [HttpHead]
+        [Route("")]
+        public IActionResult Test()
+        {
+            var response = default(IActionResult);
+            try
+            {
+                response = Ok(new
+                {
+                    StatusCode = "Up"
+                });
+            }
+            catch (Exception ex)
+            {
+                response = base.ErrorResponse(ex);
+            }
+            return response;
+        }
+    }
+}
